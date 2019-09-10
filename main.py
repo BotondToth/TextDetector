9b054321d38c3bea3a90af321ffb8ff20a0e5e9a
Tóth Botond <toth.botond@sonrisa.hu>
2019-09-10 14:13:24 +0200

    Change from character recognision to text detection

--- main.py
+++ main.py
@@ -21,25 +21,32 @@ try:
 except Exception:
     sys.exit("Invalid image")
 
-img = cv2.imread(filename)
-h, w, _ = img.shape
+data = pytesseract.image_to_data(Image.open(filename))
 
-boxes = pytesseract.image_to_boxes(img)
+image = cv2.imread(filename)
 
-# draw the bounding boxes on the image
-for b in boxes.splitlines():
-    b = b.split(' ')
-    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+lines = data.split('\n')
+for i in range(1, len(lines)):
+    cols = lines[i].split('\t')
 
-# show annotated image and wait for keypress
-cv2.imshow(filename, img)
+    confidence = int(cols[10])
+    if confidence <= 0:
+        continue
+
+    left = int(cols[6])
+    top = int(cols[7])
+    width = int(cols[8])
+    height = int(cols[9])
+
+    print(cols)
+
+    # Draw rectangles to original image
+    cv2.rectangle(image, (left, top), (left + width, top + height), (100, 100, 100), 1)
+    cv2.putText(image, cols[10] + "%", (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+    cv2.putText(image, "\"" + cols[11] + "\"", (left, top + height + 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+
+cv2.imshow("Image with detected text", image)
 cv2.waitKey(0)
-
-
-cv2.waitKey(0)
-
-
-
 cv2.destroyAllWindows()
 
 
